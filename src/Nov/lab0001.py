06b683da8a96873af3f4791ef69884e177e7f527
srinivaso421 <srinivasenv2@gmai.com>
2024-11-12 12:32:44 +0530

    Task2

---
+++ src/Nov/lab0001.py
@@ -0,0 +1,19 @@
+ # Task 1 for the Today
+# Take a 2 input from the user
+# Print the Quotient and Remainder
+# 15 ->  num1
+# 2 -> num2
+# Q -> 7
+# R -> 1
+
+num1=float(input("Enter a number"))
+num2=float(input("Enter a number:"))
+print("Quotient of the two numbers:",num1%num2)
+print(("Reminder of the number:",num1/num2))
+
+
+
+
+# Task for the Today
+# Take a 3 input from the user
+# perform the sub, sub, mul and div
