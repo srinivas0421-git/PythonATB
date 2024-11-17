646a9effe0e6bc517ba2104fb179c810f8d80e54
srinivaso421 <srinivasenv2@gmai.com>
2024-11-17 09:45:45 +0530

    12th Nov task

---
+++ src/Nov/lab006.py
@@ -0,0 +1,20 @@
+# user_age=int(input("Enter a age:"))
+#
+# if user_age>=21:
+#     print("Elgible to go club")
+# else:
+#     print("Not elgible go to club")
+#
+a=10
+if a==10:
+    print("Hello world")
+else:
+    print("not hello")
+
+#Find the max number between two numbers.
+b,c=3,4
+
+if c>=b:
+    print("c is greater than b")
+else:
+    print("b is greater than c")
