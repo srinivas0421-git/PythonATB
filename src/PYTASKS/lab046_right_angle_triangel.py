6840b4c285e2a9f61811c6b6c048ff6165d9aba3
srinivaso421 <srinivasenv2@gmai.com>
2024-12-02 06:15:43 +0530

    21st task completed

---
+++ src/PYTASKS/lab046_right_angle_triangel.py
@@ -0,0 +1,6 @@
+#Right angle triangle
+
+for i in range(1,11):
+    for j in range(i+1):
+        print("*",end="")
+    print()
---
+++ src/PYTASKS/lab047_left_angle_triangle.py
@@ -0,0 +1,8 @@
+#Right angle Triangle
+
+for i in range(1,10):
+    for k in range(10-i):
+        print(" ",end="")
+    for l in range(i):
+        print("*",end="")
+    print()
