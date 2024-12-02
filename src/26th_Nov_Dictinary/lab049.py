73bafcf3b037b8fcb7d3fd05dc5eac3b470c3d0b
srinivaso421 <srinivasenv2@gmai.com>
2024-12-02 18:38:46 +0530

    Dictinary

---
+++ src/26th_Nov_Dictinary/lab049.py
@@ -0,0 +1,37 @@
+mydict={"brand":"ceat","model":110,"year":2023}
+#Accessing dictinary
+# print(mydict)
+#Accessing dictinary using for loop
+
+# for i in mydict:
+#     print(i)
+
+#Add item into dictinary
+
+# mydict["branch"]="Hyderabad"
+# print(mydict)
+
+# print(mydict["model"])
+#
+# if "model" in mydict:
+#     print("it is existed")
+# else:
+#     print("not existed")
+#
+# print("model" in mydict)
+# print("year" not in mydict)
+#
+# print("Hyderabad" not in mydict)
+#
+# mydict.pop("model")
+# print(mydict)
+#
+# mydict["model"]="110"
+# print(mydict)
+#
+# mydict.clear()
+# print(mydict)
+mydict={"brand":"ceat","model":110,"year":2023}
+print(mydict)
+del mydict
+print(mydict)
