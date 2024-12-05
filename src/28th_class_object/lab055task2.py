2cfe6cf023008c5960073b0bd9fd0850933c8d81
srinivaso421 <srinivasenv2@gmai.com>
2024-12-05 15:48:07 +0530

    Done changes in the code

--- src/28th_class_object/lab055task2.py
+++ src/28th_class_object/lab055task2.py
@@ -8,15 +8,17 @@
 
 class api:
     def __init__(self, name, list_of_api, links):
-        self.name = None
+        self.name = name
         self.list_of_api = list_of_api
         self.links = links
 
     def list_of_apis(self):
+        print(self.name)
         print(self.list_of_api)
+
 
     def d_links(self):
         print(self.links)
-RestfulBooker = api(None,["Api","Rest_api","Soap_api"],{1,2,3,4,5})
+RestfulBooker = api("RestfulBooker",["Api","Rest_api","Soap_api"],{1,2,3,4,5})
 RestfulBooker.list_of_apis()
 RestfulBooker.d_links()
